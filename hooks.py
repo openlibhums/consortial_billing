77cb5223721ba9d68f4d509b0a51b3b8dc7ddb40
Andy Byers <andy.byers@openlibhums.org>
2017-05-10 15:49:02 +0100

    updated url

--- hooks.py
+++ hooks.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
 
 def nav_hook(context):
-	url = reverse('view_supporting_institutions')
+	url = reverse('consortial_supporters')
 	return '<li><a href="{0}">Supporting Institutions</a></li>'.format(url)
