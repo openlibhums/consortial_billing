219cc2ba0aa180127845e947be3e130b0cc83619
Martin Eve <martin@martineve.com>
2017-05-10 10:49:35 +0100

    Model rearrangement and add max_lengths

--- models.py
+++ models.py
@@ -3,25 +3,25 @@ from django.utils import timezone
 
 
 class Banding(models.Model):
-    name = models.CharField(500, blank=False, unique=True)
+    name = models.CharField(max_length=255, blank=False, unique=True)
     default_price = models.IntegerField(blank=True)
 
 
+class Institution(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    country = models.CharField(max_length=255, blank=False)
+    active = models.BooleanField(default=True)
+    consortial_billing = models.BooleanField(default=False)
+    banding = models.ForeignKey(Banding)
+
+
 class Renewal(models.Model):
-    date = models.DateField(default=timezone.now())
+    date = models.DateField(default=timezone.now)
     amount = models.IntegerField(blank=False)
-    currency = models.CharField(blank=False)
+    currency = models.CharField(max_length=255, blank=False)
     institution = models.ForeignKey(Institution)
 
 
 class BillingAgent(models.Model):
-    name = models.CharField(500, blank=False)
+    name = models.CharField(max_length=255, blank=False)
     users = models.ManyToManyField('core.Account')
-
-
-class Institution(models.Model):
-    name = models.CharField(500, blank=False)
-    country = models.CharField(500, blank=False)
-    active = models.BooleanField(default=True)
-    consortial_billing = models.BooleanField(default=False)
-    banding = models.ForeignKey(Banding)
