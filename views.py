391b86d89cda984a610795e27ecdb4134672772f
Martin Eve <martin@martineve.com>
2017-05-10 12:29:45 +0100

    File

--- views.py
+++ views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import csv
+
 
 def index(request):
-	return render(request, 'consortial_billing/admin.html', {})
+    if request.POST:
+        # an action
+        if 'import_csv' in request.POST and len(request.FILES) > 0:
+            csv_import = request.FILES[0]
+            csv_reader = csv.DictReader(csv_import.chunks, delimeter=',')
+
+            for row in csv_reader:
+                print(row["Institution Name"])
+                print(row["Renewal Amount"])
+                print(row["Currency"])
+                print(row["Renewal Date"])
+                print(row["Country"])
+                print(row["Banding"])
+                print(row["Active"])
+                print(row["Consortium"])
+                print(row["Consortial Billing"])
+                print(row["Billing Agent"])
+
+    return render(request, 'consortial_billing/admin.html', {})
