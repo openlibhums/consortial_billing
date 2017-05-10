d45f1e5d652e79ce34bb1e409a9ad5561b4d739b
Martin Eve <martin@martineve.com>
2017-05-10 11:37:08 +0100

    Fix plugins setting

--- plugin_settings.py
+++ plugin_settings.py
@@ -4,7 +4,7 @@ PLUGIN_NAME = 'Consortial Billing'
 DESCRIPTION = 'This is a plugin to handle consortial billing.'
 AUTHOR = 'Martin Paul Eve'
 VERSION = '1.0'
-SHORT_NAME = 'consortial'
+SHORT_NAME = 'consortial_billing'
 MANAGER_URL = 'consortial_index'
 
 
@@ -20,4 +20,4 @@ def install():
 def hook_registry():
     # On site load, the load function is run for each installed plugin to generate
     # a list of hooks.
-    return {'name_of_the_template_hook': 'plugins.generic_plugin.hooks.another_hook_function'}
+    return {}
