946a146c1f7646e3d65ecf1f09c9a2ef9cdc8eb7
Andy Byers <andy.byers@openlibhums.org>
2017-05-11 10:41:06 +0100

    Updated URLs

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from plugins.consortial_billing import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='consortial_index'),
+    url(r'^admin.$', views.index, name='consortial_index'),
     url(r'^non_funding_author_insts/$', views.non_funding_author_insts, name='consortial_non_funding_author_insts'),
-    url(r'^supporters/$', views.supporters, name='consortial_supporters'),
+    url(r'^$', views.supporters, name='consortial_supporters'),
 ]
