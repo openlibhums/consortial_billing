4a9224f1e3b1c7c06b3ca6be0abf986d664e5afd
Andy Byers <andy.byers@openlibhums.org>
2017-05-23 15:32:18 +0100

    Updates or consortial billing.

--- admin.py
+++ admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 
 from plugins.consortial_billing.models import *
+from core import models as core_models
+
+
+class BillingAgentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    filter_horizontal = ('users',)
+
+
+class BandingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'currency', 'default_price', 'billing_agent')
+    list_filter = ('currency', 'billing_agent')
+    search_fields = ('name',)
+
 
 admin_list = [
     (Institution, ),
-    (Banding,),
+    (Banding, BandingAdmin),
     (Renewal,),
-    (BillingAgent,),
+    (BillingAgent, BillingAgentAdmin),
     (ExcludedUser,),
     (Poll,),
     (Option,),
