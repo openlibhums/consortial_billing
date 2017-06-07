66fc44a397600265b3a42db9fe379a359fb983be
Andy Byers <andy.byers@openlibhums.org>
2017-06-07 10:18:39 +0100

    Set hook to only fire if enabled.

--- hooks.py
+++ hooks.py
@@ -10,9 +10,13 @@ def nav_hook(context):
     plugin = plugin_settings.get_self()
     short_org_name = setting_handler.get_plugin_setting(plugin, 'organisation_short_name', None, create=True,
                                                         pretty='Organisation Short Name')
+    display_nav = setting_handler.get_plugin_setting(plugin, 'display_nav', None, create=True,
+                                                        pretty='Display nav item', types='boolean').processed_value
 
-    return '<li><a href="{0}">Support {1}</a>' \
-           '<ul class="dropdown menu" data-dropdown-menu>' \
-           '<li><a href="{0}">Library Sign Up</a></li>' \
-           '<li><a href="{2}">Supporting Institutions</a></li>' \
-           '</ul></li>'.format(signup_url, short_org_name.value, supporters_url)
+    if display_nav:
+        return '<li><a href="{0}">Support {1}</a>' \
+               '<ul class="dropdown menu" data-dropdown-menu>' \
+               '<li><a href="{0}">Library Sign Up</a></li>' \
+               '<li><a href="{2}">Supporting Institutions</a></li>' \
+               '</ul></li>'.format(signup_url, short_org_name.value, supporters_url)
+    return ''
