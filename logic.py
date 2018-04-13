009bd46282d035bcb0c313e32cb940b6b0347e60
Andy Byers <andy.byers@openlibhums.org>
2018-04-13 09:44:44 +0100

    Dropped caching.

--- logic.py
+++ logic.py
@@ -186,7 +186,6 @@ def get_inst_and_poll_from_session(request):
         return None, None, False
 
 
-@function_cache.cache(30)
 def vote_count(poll):
     votes = models.Vote.objects.filter(poll=poll)
     vote_list = list()
