73143fabc0cb5333523ad136957cd40977b3d6d7
Andy Byers <andy.byers@openlibhums.org>
2018-04-16 14:12:59 +0100

    Added banding size to filters.

--- admin.py
+++ admin.py
@@ -16,7 +16,7 @@ class BillingAgentAdmin(admin.ModelAdmin):
 
 class BandingAdmin(admin.ModelAdmin):
     list_display = ('name', 'currency', 'default_price', 'billing_agent', 'display', 'size')
-    list_filter = ('currency', 'billing_agent', 'display')
+    list_filter = ('currency', 'billing_agent', 'display', 'size')
     search_fields = ('name',)
 
 
